12aacc9ed242e749bea74d5995079716877fc885
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-06 15:22:49 +0600

    :sparkles: feat: Dynamic links

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-    <router-link :to="{name: 'Jobs'}">JObs</router-link>
+    <router-link :to="{name: 'Jobs'}">Jobs</router-link>
   </nav>
   <router-view/>
 </template>
