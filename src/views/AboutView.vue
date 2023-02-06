7b94bb69f081274f3fc69e4fbe75eec68020f66f
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-06 14:29:01 +0600

    :rocket: Initial: Getting started with nos jobs

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias suscipit deserunt iure quisquam velit facere dolore. Sunt a itaque dicta corporis cum omnis unde quia, quam, eligendi dolores quis labore eos exercitationem nostrum voluptates modi eaque fugiat iure. Nesciunt aliquam incidunt, quas sint, doloribus animi quo voluptates nam minus architecto fugit iste culpa. Earum et praesentium porro saepe non repudiandae inventore quo excepturi iusto dignissimos ab nulla quasi tenetur recusandae optio totam, reiciendis iste soluta architecto? Sapiente quos ipsum aut vero omnis excepturi, ex dicta est mollitia hic distinctio dolores, impedit totam assumenda exercitationem maxime quae cumque reprehenderit! Autem, quis?</p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint magnam rerum, facere nobis, ratione quia aliquam architecto, at debitis vel nihil. Incidunt iste beatae aspernatur non provident quas ab sit illo odit dicta maxime molestias sapiente blanditiis cum quidem necessitatibus doloribus eos, dolore tempore nihil voluptatum? Rerum ut earum hic non nesciunt exercitationem dolore nostrum ipsam, quisquam aut consequuntur voluptas quam eaque deleniti blanditiis eum dicta, minus molestias a! Nesciunt, distinctio voluptatibus ullam doloremque omnis voluptas voluptatum quas ducimus, placeat veritatis, nostrum delectus blanditiis? Quos assumenda suscipit vero facere ut pariatur optio dignissimos, quae amet voluptates facilis atque autem inventore!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint magnam rerum, facere nobis, ratione quia aliquam architecto, at debitis vel nihil. Incidunt iste beatae aspernatur non provident quas ab sit illo odit dicta maxime molestias sapiente blanditiis cum quidem necessitatibus doloribus eos, dolore tempore nihil voluptatum? Rerum ut earum hic non nesciunt exercitationem dolore nostrum ipsam, quisquam aut consequuntur voluptas quam eaque deleniti blanditiis eum dicta, minus molestias a! Nesciunt, distinctio voluptatibus ullam doloremque omnis voluptas voluptatum quas ducimus, placeat veritatis, nostrum delectus blanditiis? Quos assumenda suscipit vero facere ut pariatur optio dignissimos, quae amet voluptates facilis atque autem inventore!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
