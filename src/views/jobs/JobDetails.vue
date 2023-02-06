45d3fe8eb7fa1ef0e006bf2491b884d24ed3a1c6
muhammadhafijur <hafijur.salah@gmail.com>
2023-02-06 14:57:15 +0600

    :sparkles: feat: Route parameters

---
+++ src/views/jobs/JobDetails.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Job Details Page</h1>
+        <p>The jobs id is {{ id }}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.params.id
+        }
+    }
+}
+</script>
+<style>
+    
+</style>
